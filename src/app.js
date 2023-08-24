a794b50b6230fb4502af70c4f9ca3f08801e31b1
Diego Fernandes <fernandes.diego@academico.ifpb.edu.br>
2023-08-23 21:01:21 -0300

    Tratamento de Erros

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use((req, res, next)=>{
 
 app.use(router);
 
+// Tratamento de URL não encontrada
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Endpoint not found!' });
+});
+
 module.exports = app;
